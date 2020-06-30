2f266e3994759711670abb9f9bfa1ec77e557a47
cyber <cyber.unit@gmail.com>
2020-06-30 19:28:15 +0300

    adapter for recyclerView setted

--- app/src/main/java/ru/istislav/bawp/controller/DashboardActivity.java
+++ app/src/main/java/ru/istislav/bawp/controller/DashboardActivity.java
@@ -1,17 +1,19 @@
 package ru.istislav.bawp.controller;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import android.os.Bundle;
-
 import ru.istislav.bawp.R;
+import ru.istislav.bawp.data.CourseListAdapter;
 
 public class DashboardActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private StaggeredGridLayoutManager staggeredGridLayoutManager;
+    private CourseListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,10 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         recyclerView = (RecyclerView) findViewById(R.id.courseRecyclerView);
 
+        adapter = new CourseListAdapter();
+
         staggeredGridLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
