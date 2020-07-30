6b712c95ba53e2f8f9ce285ad88332b47cb39942
CyberUnit <cyberunit@gmail.com>
2020-07-30 21:09:53 +0300

    activity_details - button "plus" and animation during textinput showing

--- app/src/main/java/ru/istislav/bawp/controller/DetailsActivity.java
+++ app/src/main/java/ru/istislav/bawp/controller/DetailsActivity.java
@@ -1,25 +1,34 @@
 package ru.istislav.bawp.controller;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import ru.istislav.bawp.R;
 import ru.istislav.bawp.data.CourseData;
 import ru.istislav.bawp.model.Course;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
     private int courseId;
     private Course course;
     private ImageView courseImageView;
     private TextView courseTitle;
     private InputMethodManager inputManager;
+    private LinearLayout revealView;
+    private boolean isEditTextVisible = false;
+    private FloatingActionButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +56,33 @@ public class DetailsActivity extends AppCompatActivity {
         inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         courseImageView = (ImageView) findViewById(R.id.detailsCourseImage);
         courseTitle = (TextView) findViewById(R.id.detailsCourseTitle);
+        revealView = (LinearLayout) findViewById(R.id.revealView);
+        revealView.setVisibility(View.INVISIBLE);
+        isEditTextVisible = false;
 
+        button = (FloatingActionButton) findViewById(R.id.detailsAddButton);
+        button.setOnClickListener(this); // Don't forget it!!!
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.detailsAddButton:
+                if(!isEditTextVisible) {
+                    revealEditText(revealView);
+                }
+                break;
+        }
+    }
+
+    private void revealEditText(LinearLayout revealView) {
+        int cx = revealView.getRight() - 30;
+        int cy = revealView.getBottom() - 60;
+
+        int finalRadius = Math.max(revealView.getWidth(), revealView.getHeight());
+        Animator anim = ViewAnimationUtils.createCircularReveal(revealView, cx, cy, 0f, finalRadius);
+        revealView.setVisibility(View.VISIBLE);
+        isEditTextVisible = true;
+        anim.start();
+    }
 }
